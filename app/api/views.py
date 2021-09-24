fbbcc636d9c71d6b15c0c95debe734e4eccd62cf
yehudaQ <79521469+yehudaQ@users.noreply.github.com>
2021-09-24 15:33:41 +0300

    DB schema

--- app/api/views.py
+++ app/api/views.py
@@ -1,4 +1,8 @@
 from flask import Blueprint
+from werkzeug.security import generate_password_hash
+
+from app import db
+from app.models.user import User
 
 views = Blueprint("views", __name__)
 
@@ -11,3 +15,14 @@ def basic():
 @views.route("/home", methods=['GET'])
 def home():
     return "Home route"
+
+
+@views.route("/test", methods=['POST'])
+def test():
+    new_user = User(email="yehuda1234@gmail.com", password=generate_password_hash(
+        "mypass", method='sha256'))
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return {}
