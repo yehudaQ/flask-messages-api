44ad263fc1478998ca30beedd8ed18311ec50b35
Yehuda Pashay <yehudap@qmasters.co>
2021-09-26 01:20:39 +0300

    Post message enhancement , serialize method for models , GET messages methods

--- app/api/message/views.py
+++ app/api/message/views.py
@@ -1,24 +1,18 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 
 from app import db
 from app.models.message import Message
+from app.models.receivedMessage import ReceivedMessage
 from app.models.user import User
 
 message = Blueprint("message", __name__)
 
 
-@message.route("/message", methods=['GET'])
-@login_required
-def basic():
-    return "Messages Views API basic route"
-
-
 @message.route("/message", methods=['GET', 'POST'])
 @login_required
-def create_message():
+def messages():
     if request.method == 'POST':
-        print(request.form)
         receiver_email = request.form.get("receiver_email")
         message = request.form.get("message")
         subject = request.form.get("subject")
@@ -28,9 +22,27 @@ def create_message():
         if not receiver:
             return 'Receiver Email is not exists.', 403
 
-        print(current_user)
         new_message = Message(message=message, subject=subject, sender_id=current_user.id)
         db.session.add(new_message)
         db.session.commit()
 
+        received_message = ReceivedMessage(message_id=new_message.id, receiver_id=receiver.id)
+        db.session.add(received_message)
+        db.session.commit()
+
         return 'Message successfully created.', 201
+
+    # GET method
+    user = User.query.filter_by(id=current_user.id).first()
+    received_messages = user.received_messages
+
+    return jsonify([msg.serialize for msg in received_messages]), 200
+
+
+@message.route("/message/sent", methods=['GET'])
+@login_required
+def get_sent_messages():
+    user = User.query.filter_by(id=current_user.id).first()
+    sent_messages = user.sent_messages
+
+    return jsonify([msg.serialize for msg in sent_messages]), 200
--- app/models/receivedMessage.py
+++ app/models/receivedMessage.py
@@ -7,3 +7,12 @@ class ReceivedMessage(db.Model):
     receiver_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     is_read = db.Column(db.Boolean, default=False)
 
+    @property
+    def serialize(self):
+        """Return object data in easily serializable format"""
+        return {
+            'id': self.id,
+            'message_id': self.message_id,
+            'receiver_id': self.receiver_id,
+            'is_read': self.is_read
+        }
